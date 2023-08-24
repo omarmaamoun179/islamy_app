85023645a1c483db15ab112a58121358d90f96c4
Omar Maamon <omarmaamoun6@gmail.com>
2023-08-24 15:57:44 +0300

    change sebhaView and add counter and change index of list when count = 33

--- lib/views/sebha_view.dart
+++ lib/views/sebha_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:islamy/widgets/custom_button.dart';
-import 'package:islamy/widgets/sebha_counter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SebhaView extends StatefulWidget {
   const SebhaView({super.key});
@@ -10,75 +9,107 @@ class SebhaView extends StatefulWidget {
 }
 
 class _SebhaViewState extends State<SebhaView> {
-  int txt = 0;
-  int txt1 = 0;
-  int txt2 = 0;
+  double angel = 0;
+  int count = 0;
+  int index = 0;
+  List<String> tsbehat = ['سبحان الله', 'الحمد لله', 'الله أكبر'];
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.asset(
-              'assets/images/Group 8.png',
-              height: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              textAlign: TextAlign.center,
-              'عدد التسبيحات',
-              style: TextStyle(color: Colors.black, fontSize: 25),
-            ),
-            SebhaCounter(txt1: txt.toString()),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-                fun: () {
-                  setState(() {
-                    txt++;
-                  });
-                },
-                text: 'سبحان الله',
-                FontFamily: 'ElMessiri'),
-            const SizedBox(
-              height: 15,
-            ),
-            SebhaCounter(txt1: txt1.toString()),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-                fun: () {
-                  setState(() {
-                    txt1++;
-                  });
-                },
-                text: 'الحمد للله',
-                FontFamily: 'ElMessiri'),
-            const SizedBox(
-              height: 15,
-            ),
-            SebhaCounter(txt1: txt2.toString()),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-                fun: () {
-                  setState(() {
-                    txt2++;
-                  });
-                },
-                text: 'الله أكبر',
-                FontFamily: 'ElMessiri')
-          ],
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        SizedBox(
+          height: h * .4,
+          width: w,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Positioned(
+                left: w * .48,
+                top: h * .05,
+                child: Image.asset(
+                  'assets/images/head of seb7a.png',
+                ),
+              ),
+              Positioned(
+                top: h * .15,
+                left: w * .29,
+                child: Transform.rotate(
+                  angle: angel,
+                  child: Image.asset(
+                    'assets/images/body of seb7a.png',
+                    height: h * .22,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        SizedBox(
+          height: h * .05,
+        ),
+        Container(
+          alignment: Alignment.center,
+          height: h * .08,
+          width: w * .15,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: const Color(0xffD0BC9F),
+          ),
+          child: Text(
+            count.toString(),
+            textAlign: TextAlign.center,
+            style: GoogleFonts.notoSansArabic(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: h * .05,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              count++;
+              setState(() {});
+              _rotate();
+              ChangeTasbeh();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xffB7935F),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            child: Text(
+              tsbehat[index],
+              style: GoogleFonts.elMessiri(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            )),
+      ],
     );
+  }
+
+  void _rotate() {
+    setState(() {
+      angel += 0.1;
+    });
+  }
+
+  void ChangeTasbeh() {
+    if (count == 34) {
+      tsbehat[index++];
+      count = 0;
+    } if(index > 2){
+      index = 0;
+    }
+    
   }
 }
